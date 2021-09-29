d658721301b428f281e50d37f5b9efa90ec0f8aa
sykn-jmn <56058545+sykn-jmn@users.noreply.github.com>
2021-09-29 13:27:09 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/registration/RegistrationController.java
@@ -0,0 +1,13 @@
+package com.example.demo.registration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+
+    public 
+}
